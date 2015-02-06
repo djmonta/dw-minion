b64307ad4e56a3234f02c0d4e33b7521ce75f7e2
monta <djmonta@gmail.com>
2015-02-06 10:12:56 +0900

    Initial Commit

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php add_filter('post_class', 'remove_hentry');
+get_header(); ?>
+<div id="primary" class="content-area">
+	<div class="primary-inner">
+		<div id="content" class="site-content" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', 'page' ); ?>
+			<?php if ( comments_open() ) comments_template(); ?>
+		<?php endwhile; ?>
+		</div>
+	</div>
+</div>
+<?php get_sidebar('secondary'); ?>
+<?php get_footer(); ?>
